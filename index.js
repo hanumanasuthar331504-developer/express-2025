53572027009c6675ea1900389efa381499b07fcc
hanumansuthar331504-developer <hanumansuthar331504@gmail.com>
2026-01-03 01:41:36 +0530

    error handling middleware

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 import express from 'express';
-import morgan from 'morgan';
 const app=express();
 
-app.use(morgan('dev'))
 app.get('/',(req,res)=>{
-    res.send('Home page')
+    res.send('Home Page')
 });
 
-app.get('/users',(req,res)=>{
-    res.send('Users pages me thoda change kia hai')
+app.get('/users',(req,res,next)=>{
+    res.send('users Page')
 });
 
-app.get("/wait",(req,res)=>{
-    setTimeout(()=>{
-        res.send('result after 1 second')
-    },1000)
+app.get('/error',(req,res,next)=>{
+const error=new Error('')
+error.status=404;
+next(error)
 });
 
+// function errorHandling (error,req,res,next){
+// res.status(error.status||500).send('Try after some time')
+// }
+
+app.use((error,req,res,next)=>{
+res.status(error.status||500).send('Try after some time')
+})
 app.listen(3200);

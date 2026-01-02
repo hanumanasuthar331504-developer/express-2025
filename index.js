9903c7a42aaa38af1a76f2409ea1a30df1860389
hanumansuthar331504-developer <hanumansuthar331504@gmail.com>
2026-01-03 03:14:22 +0530

    ejs template engine 1st basic usage

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
 import express from 'express';
 const app=express();
 
+
+app.set('view engine','ejs')
 app.get('/',(req,res)=>{
-    res.send('Home Page')
-});
-
-app.get('/users',(req,res,next)=>{
-    res.send('users Page')
-});
-
-app.get('/error',(req,res,next)=>{
-const error=new Error('')
-error.status=404;
-next(error)
-});
-
-// function errorHandling (error,req,res,next){
-// res.status(error.status||500).send('Try after some time')
-// }
-
-app.use((error,req,res,next)=>{
-res.status(error.status||500).send('Try after some time')
+res.render('home',{name:'Hanuman',ytChannel:'code step by step', age:29})
 })
-app.listen(3200);
+
+app.listen(3200)

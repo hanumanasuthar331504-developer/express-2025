01b03c0b860eb33a81b816077d923bde530abca9
hanumansuthar331504-developer <hanumansuthar331504@gmail.com>
2026-01-02 08:01:44 +0530

    route middleware

--- index.js
+++ index.js
@@ -1,40 +1,30 @@
 import express from 'express';
 const app=express();
 
-
-// function ageCheck(req,res,next){
-//     if(!req.query.age || req.query.age<18){
-//         res.send("Alert ! You can not access this page");
-//     }else{
-//         next();
-//     }
-// }
-
-// app.use(ageCheck)
-
-function ipCheck(req,res,next){
-    const ip=req.socket.remoteAddress;
-    console.log(ip);
-    if(ip.includes('192.168.1.66')){
-        res.send("Alert ! You can not access this page");
-    }else{
-        next();
-    }
+function checkAgeRouteMiddleware(req,res,next){
+if(!req.query.age || req.query.age<18){
+    res.send("You are not allowed use this website")
+}else{
+    next();
+}
 }
 
-app.use(ipCheck)
+// app.use(checkAgeRouteMiddleware);
 
-app.get("/",(req,res)=>{
-    res.send("<h1>Home page</h1>")
+app.get('',(req,res)=>{
+res.send("<h1>Home Page</h1>")
 });
 
-app.get("/login",(req,res)=>{
-    res.send("<h1>Login page</h1>")
+app.get('/login',checkAgeRouteMiddleware,(req,res)=>{
+res.send("<h1>Login Page</h1>")
 });
 
-app.get("/admin",(req,res)=>{
-    res.send("<h1>Admin page</h1>")
+app.get('/users',(req,res)=>{
+res.send("<h1>Users Page</h1>")
 });
 
+app.get('/products',checkAgeRouteMiddleware,checkAgeRouteMiddleware,(req,res)=>{
+res.send("<h1>products Page</h1>")
+});
 
 app.listen(3200);

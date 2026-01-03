5073345710b2cedcd43f3ab48529f4b630276907
hanumansuthar331504-developer <hanumansuthar331504@gmail.com>
2026-01-03 17:57:59 +0530

    Dynamic Routes making

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 import express from 'express';
-import { handleUsers } from './controller/usercontroller.js';
 const app=express();
 
+app.get('/',(req,res)=>{
+    const users=['hanuman','sam','peter','bruce'];
+    let data = `<ul>`;
+    for(let i=0;i<users.length;i++){
 
-app.set('view engine','ejs')
-app.get('/users',handleUsers);
+        data+=`<li><a href="user/${users[i]}">${users[i]}</a></li>`
+        console.log(users[i])
+    }
+    data+=`</ul>`
+    res.send(data);
+});
+
+app.get("/user/:name",(req,res)=>{
+    console.log(req.params.name);
+    const userName=req.params.name
+    res.send(`this is ${userName}'s profile page`);
+});
 
 
 app.listen(3200)    

ac2f6596a121eecfea103307c2d4b9e5e5af13cb
hanumansuthar331504-developer <hanumansuthar331504@gmail.com>
2026-01-11 02:19:10 +0530

    Make API with MongoDB

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 import express from 'express';
 import { MongoClient } from 'mongodb';
 
-const dbName="login"
-const url="mongodb://localhost:27017"
+const dbName = "login"
+const url = "mongodb://localhost:27017"
 
-const client=new MongoClient(url)
+const client = new MongoClient(url)
 
 
-const app = express()
+const app = express();
 
-app.set('view engine','ejs');
-app.set()
-app.get("/",async(req,res)=>{
-    await client.connect()
-const db = client.db(dbName);
-const collection=db.collection('users')
+app.set('view engine','ejs')
+await client.connect().then((connection) => {
+    const db = connection.db(dbName);
 
-const students= await collection.find().toArray();
-
-    res.render('students',{students})
+    app.get('/api', async(req, res) => {
+        const collection = db.collection('users')
+        const students = await collection.find().toArray();
+        res.send(students)
+    }),
+    app.get('/ui', async(req, res) => {
+        const collection = db.collection('users')
+        const students = await collection.find().toArray();
+        res.render('students',{students})
+    })
 })
+
 
 app.listen(3200)

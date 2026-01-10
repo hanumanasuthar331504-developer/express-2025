c48330b78b6602286e58b7cef8136b7f923afabe
hanumansuthar331504-developer <hanumansuthar331504@gmail.com>
2026-01-10 20:07:51 +0530

    display Mongo Data on UI

--- index.js
+++ index.js
@@ -6,18 +6,19 @@ const url="mongodb://localhost:27017"
 
 const client=new MongoClient(url)
 
-async function dbConnection(){
-await client.connect()
+
+const app = express()
+
+app.set('view engine','ejs');
+app.set()
+app.get("/",async(req,res)=>{
+    await client.connect()
 const db = client.db(dbName);
 const collection=db.collection('users')
 
-const result= await collection.find().toArray();
-console.log(result)
+const students= await collection.find().toArray();
 
-}
-
-dbConnection()
-
-const app = express()
+    res.render('students',{students})
+})
 
 app.listen(3200)

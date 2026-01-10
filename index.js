6bee2b136b7d4f9a3a2daeaebceb531ad1151d40
hanumansuthar331504-developer <hanumansuthar331504@gmail.com>
2026-01-11 04:50:03 +0530

    Save Form Data in Mongodb

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const client = new MongoClient(url)
 
 
 const app = express();
-
+app.use(express.urlencoded({extended:true}))
 app.set('view engine','ejs')
 await client.connect().then((connection) => {
     const db = connection.db(dbName);
@@ -22,6 +22,21 @@ await client.connect().then((connection) => {
         const collection = db.collection('users')
         const students = await collection.find().toArray();
         res.render('students',{students})
+    })
+
+    app.get('/add',(req,res)=>{
+        res.render('add-student')
+    })
+    
+
+    app.post('/add-student', async(req, res) => {
+        console.log(req.body);
+        const collection = db.collection('users');
+        const result = await collection.insertOne(req.body)
+        console.log(result)
+        // const students = await collection.find().toArray();
+        // res.send(students)
+        res.send("data submitted")
     })
 })
 
